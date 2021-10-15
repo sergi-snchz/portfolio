2ac5ff39cb9751c27ce0bc3411e3731344bef4db
Sergio Sánchez <sergi_snchz@protonmail.com>
2021-10-15 17:46:06 -0600

    Add tic tac toe project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import Project from "./Project";
 import calculator from "../img/calculator-4.png";
 import portfolio from "../img/portfolio.png";
 import todo from "../img/todo.png";
+import ticTacToe from "../img/tic-tac-toe.png";
 
 export default function Projects({ theme }) {
   return (
@@ -13,6 +14,7 @@ export default function Projects({ theme }) {
       </div>
       <div className="lg:flex lg:justify-between">
         <Project image={todo} title="Todo app" description="I made this todo app using JavaScript with ReactJS, CSS with TailwindCSS, NodeJS with ExpressJS and PostgreSQL as a DBMS." links={{source: "https://github.com/sergi-snchz/todo", link: "https://sergi-todo.herokuapp.com/"}} theme={ theme } />
+        <Project image={ticTacToe} title="Tic Tac Toe Game" description="I made this game using JavaScript with ReactJS and CSS with TailwindCSS." links={{source: "https://github.com/sergi-snchz/tic-tac-toe", link: "https://sergi-snchz.github.io/tic-tac-toe/"}} theme={ theme } />
       </div>
     </div>
   );
